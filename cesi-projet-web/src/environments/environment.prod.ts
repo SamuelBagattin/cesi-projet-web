471915fe87ef4a327b7bcb0128b0e2f0352f6d54
samuel <samuel.bagattin@gmail.com>
2020-04-02 09:09:59 +0200

    Fix firebase settings not present in prod env

--- cesi-projet-web/src/environments/environment.prod.ts
+++ cesi-projet-web/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCY6rcX7vqOQjQN6Kfp8R_pBxRv8gOKXgQ',
+    authDomain: 'cesi-projet-web.firebaseapp.com',
+    databaseURL: 'https://cesi-projet-web.firebaseio.com',
+    projectId: 'cesi-projet-web',
+    storageBucket: 'cesi-projet-web.appspot.com',
+    messagingSenderId: '106098798995',
+    appId: '1:106098798995:web:0e521fcea8cd54f0cc7866',
+    measurementId: 'G-QNLD6Y5MQY'
+  }
 };
